e7c8e6f31ff958ff419b4f87d74f9d4b6420efa4
dnyaneshskamthe <dnyanesh451989@gmail.com>
2023-08-08 21:02:46 +0530

    Edit and delete added

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -43,56 +43,60 @@ router.post("", upload.single('image'), async (req, res, next) => {
   
 
 // READ OPERATION
-router.get("/mypost", (req, res, next) => {
-Post.find({creator: req.userData.userId}).then(post => {
-  if (post) {
-    res.status(200).json({
-        message: "Posts fetched successfully!",
-        posts: post
-    });
-  }
-}).catch(e=>{
-    console.log(e)
-});
-});
-
+router.get("/myposts", async (req, res, next) => {
+    try {
+      // Fetch blogs created by the user
+    //   const userId = req.user.id; // Assuming you have user authentication in place
+      const userBlogs = await Post.find();
+  
+      res.status(200).json({
+        message: "User's blogs fetched successfully",
+        blogs: userBlogs
+      });
+    } catch (error) {
+      console.error('Error fetching user blogs:', error);
+      res.status(500).json({ message: 'An error occurred' });
+    }
+  });
 //UPDATE OPERATION
-router.put(
-"/:id",
-(req, res, next) => {
-    const post = new Post({
-        _id: req.body.id,
+router.put("/:id", async (req, res) => {
+    try {
+      const blogId = req.params.id;
+      const updatedData = {
         title: req.body.title,
-        content: req.body.content,
-     
+        content: req.body.content
+      };
+  
+      const updatedBlog = await Post.findByIdAndUpdate(blogId, updatedData, { new: true });
+  
+      if (updatedBlog) {
+        res.status(200).json({ message: "Blog updated successfully", post: updatedBlog });
+      } else {
+        res.status(404).json({ message: "Blog not found" });
+      }
+    } catch (error) {
+        console.error('Error updating blog:', error);
+        res.status(500).json({ message: 'An error occurred' });
+      }
     });
-    Post.updateOne(
-        { _id: req.params.id},
-        post
-      ).then(result => {
-        if(result){
-            res.status(200).json({ message: "Update successful!" });
-        }       
-        else {
-            res.status(500).json({ message: "Error Upating Post" });
-        }
-    });
-}
-);
 
 
 //DELETE OPERATION
-router.delete("/:id", (req, res, next) => {
-Post.deleteOne({ _id: req.params.id, creator: req.userData.userId }).then(
-  result => {
-    console.log(result);
-    if (result.n > 0) {
-      res.status(200).json({ message: "Deletion successful!" });
-    } else {
-        return res.status(401).json({ message: "Not authorized!!" });
+router.delete("/:id", async (req, res, next) => {
+    try {
+      const blogId = req.params.id;
+      const deletedBlog = await Post.findByIdAndDelete(blogId);
+  
+      if (deletedBlog) {
+        res.status(200).json({ message: "Blog deleted successfully" });
+      } else {
+        res.status(404).json({ message: "Blog not found" });
+      }
+    } catch (error) {
+      console.error('Error deleting blog:', error);
+      res.status(500).json({ message: 'An error occurred' });
     }
-  }
-);
-});
+  });
+
 
 module.exports = router;
